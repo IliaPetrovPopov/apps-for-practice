9951069e0cc9e11a551c7cb251ea4316b99efb2a
Ilia Popov <ilia_popov02@abv.bg>
2024-02-20 10:45:23 +0200

    Add BookStore

--- my-shelf/src/models/BookStore.ts
+++ my-shelf/src/models/BookStore.ts
@@ -1,6 +1,30 @@
 import { Book } from "./Book";
 
-class BookStore {
-  public inventory: Book[] = [];
-}
+export class BookStore {
+  private inventory: Book[] = [];
 
+  add(newBook: Book): void {
+    this.inventory.push(newBook);
+  }
+
+  remove(title: string): void {
+    this.inventory = this.inventory.filter((book) => book.title != title);
+  }
+  
+  update<T>(
+    bookToUpdate: Book,
+    detail: keyof Book,
+    value: T
+  ): void {
+    const book = this.inventory.find(
+      (book) => book.title === bookToUpdate.title
+    );
+
+    if (book) {
+      (book[detail] as T) = value;
+      console.log(`Book's ${detail} updated successfully`); 
+    } else {
+      throw new Error("Such book doesn't exist in the current Book Store!");
+    }
+  }
+}
