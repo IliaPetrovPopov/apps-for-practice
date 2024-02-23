9215c43818f41e20b93e00cb34bcc536c4fa96a5
Ilia Popov <ilia_popov02@abv.bg>
2024-02-23 20:39:05 +0200

    Add manual testing inside main file index.ts

--- gify/src/index.ts
+++ gify/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello");
+import { Gify } from "./models/Gify.js";
+import { Amazon } from "./models/specificBrands/Amazon.js";
+import { Apple } from "./models/specificBrands/Apple.js";
+import { Starbucks } from "./models/specificBrands/Starbucks.js";
+
+const apple = new Apple();
+const starbucks = new Starbucks();
+const amazon = new Amazon();
+
+const gify = new Gify([apple, starbucks], []);
+// const meow = new Meow();
+
+// gify.addNewCustomer(meow)
+
+// meow.requestGiftCards(apple, 3)
+// gify.checkCustomer("meow")
