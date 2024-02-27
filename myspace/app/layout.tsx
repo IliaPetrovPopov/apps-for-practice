097dabf30ff4df3d93eefb614c950ab44b84bed7
Ilia Popov <ilia_popov02@abv.bg>
2024-02-27 20:04:02 +0200

    Add basic layout with NavMenu

--- myspace/app/layout.tsx
+++ myspace/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import NavMenu from "@/components/NavMenu/NavMenu";
+
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <NavMenu />
+        {children}
+      </body>
     </html>
   );
 }
