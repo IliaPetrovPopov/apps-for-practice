a3194c358e12983720bfb001129f49e1f7c924cc
Ilia Popov <ilia_popov02@abv.bg>
2024-02-29 19:15:02 +0200

    Modify Home page

--- myspace/app/page.tsx
+++ myspace/app/page.tsx
@@ -1,5 +1,14 @@
+import { getServerSession } from "next-auth";
 import styles from "./page.module.css";
+import { redirect } from "next/navigation";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
   return <main className={styles.main}></main>;
 }
