e3e285d8bafdb547a188697ed7246a8f172803d5
Ilia Popov <ilia_popov02@abv.bg>
2024-02-29 19:14:15 +0200

    Add dashboard & ProfileForm on it

---
+++ myspace/app/dashboard/page.tsx
@@ -0,0 +1,27 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import { prisma } from "@/lib/prisma";
+import ProfileForm from "./ProfileForm";
+
+async function Dashboard() {
+  const session = await getServerSession(authOptions);
+
+  console.log(session);
+  
+  if (!session) redirect("/api/auth/signin");
+
+  const currentUserEmail = session?.user?.email!;
+  const user = await prisma.user.findUnique({
+    where: {
+      email: currentUserEmail,
+    },
+  });
+  return (
+    <div>
+      <ProfileForm user={user} />
+    </div>
+  );
+}
+
+export default Dashboard;
