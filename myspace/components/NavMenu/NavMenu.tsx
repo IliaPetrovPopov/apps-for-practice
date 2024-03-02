bc6fc720ba40acb7d76fa3937c9d7d024c2af685
Ilia Popov <ilia_popov02@abv.bg>
2024-03-02 13:45:29 +0200

    Fix error with showing the site logo

--- myspace/components/NavMenu/NavMenu.tsx
+++ myspace/components/NavMenu/NavMenu.tsx
@@ -9,19 +9,19 @@ const NavMenu = () => {
   return (
     <nav className={styles.nav}>
       <Link href="/">
-        <Image src="../../public/logo.svg" width={216} height={30} alt="MySpace Logo" />
+        <Image src="/logo.svg" width={216} height={30} alt="MySpace Logo" />
       </Link>
       <ul className={styles.links}>
         <li>
-        <Link href={'/about'}>About</Link>
+          <Link href={"/about"}>About</Link>
         </li>
 
         <li>
-        <Link href={'/blog'}>Blog</Link>
+          <Link href={"/blog"}>Blog</Link>
         </li>
 
         <li>
-        <Link href={'/users'}>Users</Link>
+          <Link href={"/users"}>Users</Link>
         </li>
         <li>
           <SignInButton />
